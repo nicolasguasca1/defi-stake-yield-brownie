ca31c2adbbc677d0bbc008adf2ee0da3a4a98d32
Nicolas Guasca <nicolas.guasca@gmail.com>
2021-11-09 16:48:09 +0100

    +1

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DappToken is ERC20 {
-    constructor() ERC20("DappToken", "DAPP"){
-        _mint(msg.sender, 1000000000000000000);
+    constructor() public ERC20("Dapp Token", "DAPP"){
+        _mint(msg.sender, 1000000000000000000000000);
     }
 }
--- scripts/deploy.py
+++ scripts/deploy.py
@@ -10,12 +10,15 @@ def deploy_token_farm_and_dapp_token():
     dapp_token = DappToken.deploy({"from": account})
     token_farm = TokenFarm.deploy(
         dapp_token.address,
-        {"from": account}, publish_source=config["networks"][network.show_active()]["verify"],
+        {"from": account},
+        publish_source=config["networks"][network.show_active()]["verify"],
     )
     tx = dapp_token.transfer(
-        token_farm.address, dapp_token.totalSupply() - KEPT_BALANCE, {"from": account})
+        token_farm.address, dapp_token.totalSupply() -
+        KEPT_BALANCE, {"from": account}
+    )
     tx.wait(1)
-    # dapp_token, weth_token, fau_token as dai
+    # dapp_token, weth_token, fau_token/dai
     weth_token = get_contract("weth_token")
     fau_token = get_contract("fau_token")
     dict_of_allowed_tokens = {
@@ -24,15 +27,18 @@ def deploy_token_farm_and_dapp_token():
         weth_token: get_contract("eth_usd_price_feed"),
     }
     add_allowed_tokens(token_farm, dict_of_allowed_tokens, account)
+    # if front_end_update:
+    #     update_front_end()
     return token_farm, dapp_token
 
 
 def add_allowed_tokens(token_farm, dict_of_allowed_tokens, account):
     for token in dict_of_allowed_tokens:
-        add_tx = token_farm.addAllowedToken(token.address, {"from": account})
+        add_tx = token_farm.addAllowedTokens(token.address, {"from": account})
         add_tx.wait(1)
         set_tx = token_farm.setPriceFeedContract(
-            token.address, dict_of_allowed_tokens[token], {"from": account})
+            token.address, dict_of_allowed_tokens[token], {"from": account}
+        )
         set_tx.wait(1)
     return token_farm
 
