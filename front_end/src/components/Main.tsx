f211bc138fc3daa47c1011ec2347355af6736bd9
Nicolas Guasca <nicolas.guasca@gmail.com>
2021-11-11 23:21:19 +0100

    Wallet balances

---
+++ front_end/src/components/Main.tsx
@@ -0,0 +1,58 @@
+/* eslint-disable spaced-comment */
+/// <reference types="react-scripts" />
+import { useEthers } from "@usedapp/core";
+import helperConfig from "../helper-config.json";
+import networkMapping from "../chain-info/deployments/map.json";
+import { constants } from "ethers";
+import brownieConfig from "../brownie-config.json";
+import dapp from "../dapp.png";
+import eth from "../eth.png";
+import fau from "../dai.png";
+
+import { YourWallet } from "./yourWallet";
+
+export type Token = {
+  image: string;
+  address: string;
+  name: string;
+};
+
+const Main = () => {
+  // Show token values
+  // Get the address of different tokens
+  // Get the balance of the users wallet
+  // Send our brownie config to the front-end
+  const { chainId } = useEthers();
+  const networkName = chainId ? helperConfig[chainId] : "dev";
+  console.log(chainId);
+  console.log(networkName);
+  const dappTokenAddress = chainId
+    ? networkMapping[String(chainId)]["DappToken"][0]
+    : constants.AddressZero;
+  const wethTokenAddress = chainId
+    ? brownieConfig["networks"][networkName]["weth_token"]
+    : constants.AddressZero;
+  const fauTokenAddress = chainId
+    ? brownieConfig["networks"][networkName]["fau_token"]
+    : constants.AddressZero;
+  const supportedTokens: Array<Token> = [
+    {
+      image: dapp,
+      address: dappTokenAddress,
+      name: "DAPP"
+    },
+    {
+      image: eth,
+      address: wethTokenAddress,
+      name: "WETH"
+    },
+    {
+      image: fau,
+      address: fauTokenAddress,
+      name: "DAI"
+    }
+  ];
+  return <YourWallet supportedTokens={supportedTokens} />;
+};
+
+export default Main;
---
+++ front_end/src/components/yourWallet/YourWallet.tsx
@@ -0,0 +1,38 @@
+import { Token } from "../Main";
+import { useState, useEffect } from "react";
+import { Box, Tab } from "@material-ui/core";
+import { TabContext, TabList, TabPanel } from "@material-ui/lab";
+import { WalletBalance } from "./WalletBalance";
+
+interface YourWalletProps {
+  supportedTokens: Array<Token>;
+}
+
+export const YourWallet = ({ supportedTokens }: YourWalletProps) => {
+  const [selectedTokenIndex, setSelectedTokenIndex] = useState<number>(0);
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setSelectedTokenIndex(Number(newValue));
+  };
+  return (
+    <Box>
+      <h1 className="your-wallet">Your wallet</h1>
+      <Box>
+        <TabContext value={selectedTokenIndex.toString()}>
+          <TabList aria-label="stake form tabs" onChange={handleChange}>
+            {supportedTokens.map((token, index) => (
+              <Tab key={index} label={token.name} value={index.toString()} />
+            ))}
+          </TabList>
+          {supportedTokens.map((token, index) => {
+            return (
+              <TabPanel key={index} value={index.toString()}>
+                <WalletBalance token={supportedTokens[selectedTokenIndex]} />
+              </TabPanel>
+            );
+          })}
+        </TabContext>
+      </Box>
+    </Box>
+  );
+};
